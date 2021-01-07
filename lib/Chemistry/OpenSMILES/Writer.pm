9ce00fc88468b4e308c8359c962e80d94fa641c0
Andrius Merkys <andrius.merkys@gmail.com>
2021-01-07 16:22:12 +0200

    Handling ring bonds.

--- lib/Chemistry/OpenSMILES/Writer.pm
+++ lib/Chemistry/OpenSMILES/Writer.pm
@@ -12,51 +12,45 @@ sub write
 {
     my( $graph ) = @_;
 
-    my $operations = {
-        tree_edge     => \&_tree_edge,
-        non_tree_edge => \&_non_tree_edge,
+    my @symbols = ( '(' );
+    my %vertex_symbols;
+    my $nrings = 0;
+    my %seen_rings;
 
-        pre  => \&_pre_vertex,
-        post => \&_post_vertex,
+    my $operations = {
+        tree_edge     => sub { push @symbols, _tree_edge( @_ ) },
+        non_tree_edge => sub { return if $seen_rings{join '|', sort @_[0..1]};
+                               $nrings++;
+                               ${$vertex_symbols{$_[0]}} .= $nrings;
+                               ${$vertex_symbols{$_[1]}} .= $nrings;
+                               $seen_rings{join '|', sort @_[0..1]} = 1; },
+
+        pre  => sub { push @symbols, _pre_vertex( @_ );
+                      $vertex_symbols{$_[0]} = \$symbols[-1] },
+        post => sub { push @symbols, ')' },
     };
 
     my $traversal = Graph::Traversal::DFS->new( $graph, %$operations );
-    print '(';
     $traversal->dfs;
+    return join '', @symbols;
 }
 
 sub _tree_edge
 {
     my( $u, $v, $self ) = @_;
 
-    print '(';
-
     my $graph = $self->graph;
-    return unless $graph->has_edge_attribute( $u, $v, 'bond' );
+    return '(' unless $graph->has_edge_attribute( $u, $v, 'bond' );
 
     # CAVEAT: '/' and '\' bonds are problematic
-    print $graph->get_edge_attribute( $u, $v, 'bond' );
-}
-
-sub _non_tree_edge
-{
-    my( $u, $v, $self ) = @_;
-
-    # TODO: handle rings
+    return '(' . $graph->get_edge_attribute( $u, $v, 'bond' );
 }
 
 sub _pre_vertex
 {
     my( $vertex, $self ) = @_;
 
-    print $vertex->{symbol};
-}
-
-sub _post_vertex
-{
-    my( $vertex, $self ) = @_;
-
-    print ')';
+    return $vertex->{symbol};
 }
 
 1;
