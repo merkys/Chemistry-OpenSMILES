e512c897a5befee01384f54fa99a47f0040b3741
Andrius Merkys <andrius.merkys@gmail.com>
2021-04-30 08:29:59 +0300

    Finishing the code for SSSR::BFS.

--- lib/SSSR/BFS.pm
+++ lib/SSSR/BFS.pm
@@ -7,50 +7,80 @@ sub get_SSSR
 {
     my( $graph ) = @_;
 
+    my @cycles;
+
     for my $original_vertex ($graph->vertices) {
         my @current_stage = ( $original_vertex );
         my @next_stage;
-        my %current_stage = ( $original_vertex => 0 );
-        my %next_stage;
-        my %paths = ( $original_vertex => [ $original_vertex ] );
+        my %paths = ( $original_vertex => [ [ $original_vertex ] ] );
         my %opened = ( $original_vertex => 1 );
         my %visited;
 
         while( @current_stage ) {
             for my $vertex (@current_stage) {
-                next unless defined $vertex; # might be undefined
-                next if $visited{$vertex};   # might be already visited
+                next if $visited{$vertex}; # might be already visited
                 $visited{$vertex} = 1;
                 for my $neighbour ($graph->neighbours($vertex)) {
                     if( $visited{$neighbour} ) {
-                        if( grep { $_ eq $neighbour } @{$paths->{$vertex}} ) {
-                            # Found a vertex already in my path, nothing to do
-                        } else {
-                            # Closing a cycle unless there are common members
-                            # TODO: report a cycle
+                        for my $path (@{$paths{$neighbour}}) {
+                            my @common = common( $paths{$vertex}, $path );
+                            next if scalar @common != 1;
+                            push @cycles,
+                                 [ @{$paths{$vertex}}, reverse @path ];
+                            pop @{$cycles[-1]};
                         }
+                        # Recording a new path to the vertex
+                        push @{$paths{$neighbour}},
+                             [ $paths{$vertex}, $neighbour ];
                     } else {
-                        if( exists $next_stage{$neighbour} ) {
-                            # Closing a cycle unless there are common members,
-                            # no need to go any further
-                            # TODO: check and report a cycle
+                        if( $opened{$neighbour} ) {
+                            for my $path (@{$paths{$neighbour}}) {
+                                my @common = common( $paths{$vertex},
+                                                     $path );
+                                next if scalar @common != 1;
+                                push @cycles,
+                                     [ @{$paths{$vertex}}, reverse @path ];
+                                pop @{$cycles[-1]};
+                            }
                             # No need to visit anything past this point
                             $visited{$neighbour} = 1;
                         } else {
                             # Put vertex in the queue for next iteration
                             $next_stage{$neighbour} = scalar @next_stage;
-                            push @next_stage, $neighbour;
-                            @{$paths{$neighbour}} = @{$paths{$vertex}}, $neighbour;
+                            @{$paths{$neighbour}} = @{$paths->{$vertex}},
+                                                    $neighbour;
+                            $opened{$neighbour} = 1;
                         }
                     }
                 }
             }
             @current_stage = @next_stage;
-            %current_stage = %next_stage;
             @next_stage = ();
-            %next_stage = ();
         }
     }
+}
+
+sub common
+{
+    my( $A, $B ) = @_;
+
+    my @A = sort @$A;
+    my @B = sort @$B;
+
+    my @common;
+    while( @A || @B ) {
+        if( $A[0] lt $B[0] ) {
+            shift @A;
+        } elsif( $A[0] gt $B[0] ) {
+            shift @B;
+        } else {
+            push @common, $A[0];
+            shift @A;
+            shift @B;
+        }
+    }
+
+    return @common;
 }
 
 1;
